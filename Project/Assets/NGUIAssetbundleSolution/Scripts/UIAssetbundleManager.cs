e9a82e05b9c6048dd2a03b4fae9271c864bf29f7
sFisherE <397381741@qq.com>
2013-12-31 15:31:12 +0800

    some logic

--- Project/Assets/NGUIAssetbundleSolution/Scripts/UIAssetbundleManager.cs
+++ Project/Assets/NGUIAssetbundleSolution/Scripts/UIAssetbundleManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 /// <summary>
 ///   the unload stategy: i am thinking
+///   how to download ui form internet
 /// </summary>
 public class UIAssetbundleManager:MonoBehaviour
 {
@@ -28,6 +29,8 @@ public class UIAssetbundleManager:MonoBehaviour
         }
     }
     public List<Texture> loadedTextures = new List<Texture>();
+    public List<AudioClip> loadedAudioClips = new List<AudioClip>();
+    public List<Font> loadedFonts = new List<Font>();
     public void LoadUIAssetbundle(string name)
     {
         StartCoroutine(CoLoadUIAssetbundle(name));
@@ -44,7 +47,7 @@ public class UIAssetbundleManager:MonoBehaviour
                 //the texture name should be unique
                 if (loadedTextures.Find(p => p.name == texName) == null)
                 {
-                    string fullPath = UIAssetbundleSettings.texturePathUrl + path;
+                    string fullPath = UIAssetbundleSettings.texturePathUrl + "/" + path;
                     WWW ret = new WWW(fullPath);
                     yield return ret;
                     Texture tex = ret.assetBundle.Load(texName, typeof(Texture)) as Texture;
@@ -52,21 +55,57 @@ public class UIAssetbundleManager:MonoBehaviour
                 }
             }
         }
+        //load font
+        if (dep.fontPaths != null && dep.fontPaths.Count > 0)
+        {
+            foreach (var path in dep.fontPaths)
+            {
+                string fontName = path.Substring(0, path.Length - UIAssetbundleSettings.ext.Length);
+                if (loadedFonts.Find(p => p.name == fontName) == null)
+                {
+                    string fullPath = UIAssetbundleSettings.fontPathUrl+"/" + path;
+                    WWW ret = new WWW(fullPath);
+                    yield return ret;
+                    Font font = ret.assetBundle.Load(fontName, typeof(Font)) as Font;
+                    loadedFonts.Add(font);
+                }
+            }
+        }
+        //load audioClip
+        if (dep.audioPaths != null && dep.audioPaths.Count > 0)
+        {
+            foreach (var path in dep.audioPaths)
+            {
+                string audioName = path.Substring(0, path.Length - UIAssetbundleSettings.ext.Length);
+                if (loadedAudioClips.Find(p => p.name == audioName) == null)
+                {
+                    string fullPath = UIAssetbundleSettings.audioPathUrl + "/" + path;
+                    WWW ret = new WWW(fullPath);
+                    yield return ret;
+                    AudioClip audioClip = ret.assetBundle.Load(audioName, typeof(AudioClip)) as AudioClip;
+                    loadedAudioClips.Add(audioClip);
+                }
+            }
+        }
         //then load the prefab
-        WWW bundle = new WWW(UIAssetbundleSettings.pathUrl + name + UIAssetbundleSettings.ext);
+        WWW bundle = new WWW(UIAssetbundleSettings.pathUrl + "/" + name + UIAssetbundleSettings.ext);
         yield return bundle;
         GameObject go = Instantiate(bundle.assetBundle.mainAsset) as GameObject;
         go.transform.parent = root;
         go.transform.localScale = Vector3.one;
+        go.transform.localPosition = Vector3.zero;
+
+        //set UIAnchor's camera
+
         bundle.assetBundle.Unload(false);
     }
 
 
 
     //just for test
-#if UNITY_EDITOR
     public string name;
     public Transform root;
+#if UNITY_EDITOR
     void OnGUI()
     {
         if (GUILayout.Button("Test",GUILayout.Width(100),GUILayout.Height(50)))
